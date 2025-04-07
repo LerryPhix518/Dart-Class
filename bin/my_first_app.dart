6b0dac1b992218626a4ced44dce4625bfad16f91
unknown <lerryphix518@gmail.com>
2025-04-07 02:25:09 -0700

    bin added

---
+++ bin/my_first_app.dart
@@ -0,0 +1,12 @@
+//returnType functionName(){}
+
+void main(){
+print("Hello Everyone,\n");
+print("My name is Abayomi. I just started my Mobile App journey at Appclick Academy, where I'm learning how to build Mobile Application using Flutter, Dart right from vscode(Virtual Studio Code.\n");
+print("My aim is to be able to use effectively Flutter, Dart, VS Code, Android Studio and become a Mobile App Developer and also a Google Developer Expert in Flutter.\n");
+print("My Tutor's name is Mr Ademola Ibukunoluwa, who is a Mobile App Developer and a Google Developer Expert in Flutter.\n");
+print("I am so excited to be here as we have a great and conducive learning environment and our Tutor is so friendly and approachable.\n");
+print("I am looking forward to learning more about Flutter and Dart, etc and also looking forward to building my first Mobile Application.\n");
+print("This will be all for now\n");
+print("Thank you for your time.");
+}
